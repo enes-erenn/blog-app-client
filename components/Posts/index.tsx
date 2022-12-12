0d55c7fb6b9730f9809e3becbf37e2e7a73175be
enes-erenn <thenesern@gmail.com>
2022-12-12 23:56:06 +0300

    Home and Write pages style improvements

--- components/Posts/index.tsx
+++ components/Posts/index.tsx
@@ -8,19 +8,18 @@ import styles from "../../styles/Posts.module.scss";
 
 const Posts = () => {
   const [posts, setPosts] = useState<PostType[]>([]);
-  const category = useRouter().query;
 
   useEffect(() => {
     const getData = async () => {
       try {
-        const res = await axios.get(process.env.API_URL + `/posts${category}`);
+        const res = await axios.get(process.env.API_URL + `/posts`);
         setPosts(res.data);
       } catch (err) {
         console.log(err);
       }
     };
     getData();
-  }, [category]);
+  }, []);
 
   const getText = (html: string | undefined) => {
     if (html) {
@@ -34,7 +33,12 @@ const Posts = () => {
       {posts.map((post: PostType) => (
         <div key={post.id} className={styles.post}>
           <div className={styles.img_container}>
-            <Image src="" alt="Post" />
+            <Image
+              src={post?.img || ""}
+              alt="Post"
+              layout="fill"
+              objectFit="cover"
+            />
           </div>
           <div className={styles.content}>
             <Link href={`/post/${post.id}`}>
--- pages/write.tsx
+++ pages/write.tsx
@@ -1,17 +1,17 @@
 import React, { useState } from "react";
 import dynamic from "next/dynamic";
+const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
 import { PostType } from "../types/types";
 import "react-quill/dist/quill.snow.css";
-import styles from "../styles/Write.module.scss";
 import axios from "axios";
 import moment from "moment";
+import styles from "../styles/Write.module.scss";
 
 const Write = () => {
-  const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
   const [value, setValue] = useState("");
+  const [rawImage, setRawImage] = useState<File | null>(null);
   const [post, setPost] = useState<PostType>({
     title: "",
-    img: "",
     category: "",
   });
   const [isUpdating, setIsUpdating] = useState<Boolean>(false);
@@ -21,6 +21,20 @@ const Write = () => {
   };
 
   const handleSubmit = async () => {
+    let image;
+    if (rawImage) {
+      const data = new FormData();
+      data.append("file", rawImage);
+      data.append("upload_preset", "uploads");
+      let uploadRes;
+      if (data) {
+        uploadRes = await axios.post(
+          "https://api.cloudinary.com/v1_1/dnlxubidd/image/upload",
+          data
+        );
+        image = uploadRes?.data?.url;
+      }
+    }
     try {
       if (isUpdating) {
         await axios.patch(process.env.API_URL + `/posts/${post.id}`, {
@@ -33,12 +47,16 @@ const Write = () => {
           {
             ...post,
             desc: value,
+            img: image,
             date: moment(Date.now()).format("YYYY-MM-DD HH:mm:ss"),
           },
           {
             withCredentials: true,
           }
         );
+        setRawImage(null);
+        setValue("");
+        setPost({ category: "", title: "" });
       }
     } catch (err) {
       console.log(err);
@@ -53,6 +71,7 @@ const Write = () => {
           placeholder="Title"
           onChange={changeHandler}
           name="title"
+          value={post?.title}
         />
         <div className={styles.editor_container}>
           <ReactQuill
@@ -65,17 +84,38 @@ const Write = () => {
       </div>
       <div className={styles.menu}>
         <div className={styles.item}>
-          <h1>Publish</h1>
-          <span>
-            <b>Status: </b> Draft
-          </span>
-          <span>
-            <b>Visibility: </b> Public
-          </span>
-          <input style={{ display: "none" }} type="file" id="file" name="" />
-          <label className={styles.file} htmlFor="file">
-            Upload
-          </label>
+          <div className={styles.info}>
+            <h1>Publish</h1>
+            <span>
+              <b>Status: </b> Draft
+            </span>
+            <span>
+              <b>Visibility: </b> Public
+            </span>
+            <input
+              style={{ display: "none" }}
+              type="file"
+              id="file"
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                if (e.target.files) {
+                  setRawImage(e.target.files[0]);
+                }
+              }}
+            />
+            <div
+              style={{
+                display: "flex",
+                gap: "1rem",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <label className={styles.file} htmlFor="file">
+                Upload Image
+              </label>
+              <p style={{ color: "lightgray" }}>{rawImage && "Loaded"}</p>
+            </div>
+          </div>
           <div className={styles.buttons}>
             <button>Save as a draft</button>
             <button onClick={handleSubmit}>Publish</button>
@@ -84,14 +124,39 @@ const Write = () => {
         <div className={styles.item}>
           <div>
             <h1>Category</h1>
-            <input
-              type="radio"
-              name="category"
-              value="art"
-              id="art"
-              onChange={changeHandler}
-            />
-            <label htmlFor="art">Art</label>
+            <div className={styles.category}>
+              <input
+                type="radio"
+                name="category"
+                value="Software Development"
+                id="software_development"
+                checked={post?.category == "Software Development"}
+                onChange={changeHandler}
+              />
+              <label htmlFor="software_development">Software Development</label>
+            </div>
+            <div className={styles.category}>
+              <input
+                type="radio"
+                name="category"
+                value="Personal Development"
+                checked={post?.category == "Personal Development"}
+                id="personal_development"
+                onChange={changeHandler}
+              />
+              <label htmlFor="personal_development">Personal Development</label>
+            </div>
+            <div className={styles.category}>
+              <input
+                type="radio"
+                name="category"
+                value="Design"
+                id="design"
+                checked={post?.category == "Design"}
+                onChange={changeHandler}
+              />
+              <label htmlFor="design">Design</label>
+            </div>
           </div>
         </div>
       </div>
